4e64e7fe0eae2f22c07756a8d8cdd67b80241a1c
webmilio <therealwebmilio@gmail.com>
2020-09-18 22:03:33 -0400

    Marged all basic presents into PresentEntry.

--- Entries/Present/PresentEntry.cs
+++ Entries/Present/PresentEntry.cs
@@ -1,4 +1,8 @@
-﻿namespace PresentOpener.Entries.Present
+﻿using Terraria;
+using Terraria.ID;
+using static Terraria.ID.ItemID;
+
+namespace PresentOpener.Entries.Present
 {
     public abstract class PresentEntry : ItemEntry
     {
@@ -13,5 +17,124 @@
         }
 
         public abstract float GetConfigurationMultiplier(PresentOpenerConfig.PresentMenu presentMenu);
+    }
+
+    public class CoalEntry : PresentEntry
+    {
+        public CoalEntry() : base(Coal, 30)
+        {
+        }
+
+
+        public override float GetConfigurationMultiplier(PresentOpenerConfig.PresentMenu presentMenu)
+        {
+            return presentMenu.CoalScale;
+        }
+    }
+
+    public class DogWhistleEntry : PresentEntry
+    {
+        public DogWhistleEntry() : base(DogWhistle, 419)
+        {
+        }
+
+
+        public override float GetConfigurationMultiplier(PresentOpenerConfig.PresentMenu presentMenu)
+        {
+            return presentMenu.WhistleScale;
+        }
+    }
+
+    public class EggnogEntry : PresentEntry
+    {
+        public EggnogEntry() : base(Eggnog, 12)
+        {
+        }
+
+
+        public override float GetConfigurationMultiplier(PresentOpenerConfig.PresentMenu presentMenu)
+        {
+            return presentMenu.EggnogScale;
+        }
+
+        public override int GetSpawnCount(ItemEntryType type, Player player)
+        {
+            return Main.rand.Next(1, 4);
+        }
+    }
+
+    public class HollyEntry : PresentEntry
+    {
+        public HollyEntry() : base(Holly, 11)
+        {
+        }
+
+
+        public override float GetConfigurationMultiplier(PresentOpenerConfig.PresentMenu presentMenu)
+        {
+            return presentMenu.HollyScale;
+        }
+    }
+
+    public class ReindeerAntlersEntry : PresentEntry
+    {
+        public ReindeerAntlersEntry() : base(ReindeerAntlers, 43)
+        {
+        }
+
+
+        public override float GetConfigurationMultiplier(PresentOpenerConfig.PresentMenu presentMenu)
+        {
+            return presentMenu.AntlersScale;
+        }
+    }
+
+    public class SnowGlobeEntry : PresentEntry
+    {
+        public SnowGlobeEntry() : base(SnowGlobe, 5)
+        {
+        }
+
+
+        public override float GetConfigurationMultiplier(PresentOpenerConfig.PresentMenu presentMenu)
+        {
+            return presentMenu.SnowGlobeScale;
+        }
+
+        public override bool CanObtain(ItemEntryType type, Player player)
+        {
+            return Main.hardMode;
+        }
+    }
+
+    public class StarAniseEntry : PresentEntry
+    {
+        public StarAniseEntry() : base(StarAnise, 16)
+        {
+        }
+
+
+        public override float GetConfigurationMultiplier(PresentOpenerConfig.PresentMenu presentMenu)
+        {
+            return presentMenu.StarAniseScale;
+        }
+
+        public override int GetSpawnCount(ItemEntryType type, Player player)
+        {
+            return Main.rand.Next(20, 41);
+        }
+    }
+
+    public class ToolboxEntry : PresentEntry
+    {
+        public ToolboxEntry() : base(Toolbox, 323)
+        {
+        }
+
+
+        public override float GetConfigurationMultiplier(PresentOpenerConfig.PresentMenu presentMenu)
+        {
+            return presentMenu.ToolboxScale;
+        }
     }
 }
